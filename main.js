93cbf0d7ab58b59a6ef526b79ba4912c36beb171
nxrada <56482517+nxrada@users.noreply.github.com>
2020-02-27 21:41:24 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ var gameData = {
     tokes: 0,
     tokesPerClick: 1,
     budPerClickCost: 10,
-    dinero: 0,
+    debt: 0,
     }
     
     function takeAToke() {
     gameData.tokes += 1
-    document.getElementById("tokesTaken").innerHTML = gameData.tokes + " Tokes"
+    document.getElementById("tokesTaken").innerHTML = "Tokes: " + gameData.tokes 
     }
     
     function buyBudPerClick() {
@@ -18,6 +18,8 @@ var gameData = {
     document.getElementById("budCostTracker").innerHTML = "(Currently " + gameData.tokesPerClick + "grams; Cost: $" + gameData.budPerClickCost + ")"
     }
     }
+   
+    
     
     var saveGameLoop = window.setInterval(function() {
     localStorage.setItem('tokeSeshSave', JSON.stringify(gameData))
