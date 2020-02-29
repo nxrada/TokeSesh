b698266e78976d5f921ec5d22b9ec45cde658827
nxrada <56482517+nxrada@users.noreply.github.com>
2020-02-29 10:40:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var gameData = 
-    {
+var gameData = {
         tokes: 0,
         tokesRemaining: 64,
         tokesPerClick: 1,
@@ -12,56 +11,52 @@ var gameData =
     }
 //Declaration of gameData variables.
 
-function takeAToke() 
-{
+function takeAToke() {
         gameData.tokes ++;
         gameData.tokesRemaining --;
         document.getElementById("tokesTaken").innerHTML = "Tokes: " + gameData.tokes; 
 }
 //Function which runs on "Take a Toke" button click.
 
-function rollOne();
-{
+function rollOne() {
     gameData.joints++;
     gameData.budLeft--;
     document.getElementById("jointCounter").innerHTML = "J's rolled: " + gameData.joints;
+    document.getElementById("budCount").innerHTML = "Bud left:" + gameData.budLeft + "g";
 }
 //Rolls a joint.
 
-function buyBud() 
-{
+function buyBud() {
         gameData.budPerClick += 1;
         gameData.budPerClick *= 2;
         budLeft += 28;
         debt += 250;
-        document.getElementById("debtCount").innerHTML = "Current debt: $" + gameData.debt;
+        document.getElementById("debtCount").innerHTML = "Current debt: " + gameData.debt +"dollars;
+        document.getElementById("budCount").innerHTML = "Bud left: " + gameData.budLeft + "grams";
 }
 //Buys an ounce of bud.
 
-function debtAccumulation() 
-{
+function debtAccumulation() {
     gameData.debt+= debtAccumulationFactor;
     document.getElementById("debtCount").innerHTML = "Current debt: $" + gameData.debt;
 }
 //Function in which you accumulate debt.
 
-var debtLoop = window.setInterval(function() 
-{
+var debtLoop = window.setInterval(function() {
     debtAccumulation();
 }, 10000)
 //Debt loop.
 
-var saveGameLoop = window.setInterval(function() 
-{
+var saveGameLoop = window.setInterval(function() {
     localStorage.setItem('tokeSeshSave', JSON.stringify(gameData));
 }, 15000)
 //Savegame loop for saving the game
 
+
 var savegame = JSON.parse(localStorage.getItem("tokeSeshSave"))
 //Savegame variable.
 
-if (savegame !== null) 
-{
+if (savegame !== null) {
     gameData = savegame;
 }
 //If statement that loads savegame. 
