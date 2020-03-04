5ca589d1974a7344d02f8e4a7938ffc7f453ef71
nxrada <56482517+nxrada@users.noreply.github.com>
2020-03-03 19:20:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ var gameData = {
         wealth: 0,
         budLeft: 1,
         joints: 0,
-        shiftsRemaining: 60,
-        salary: 15,
-        promotionTracker: 0
+        shiftsRemaining: 10,
+        salary: 8,
+        autoTokeCount: 0, 
+        autoTokeCost: 4,
+        promotionTracker: 0,
 }
 //--------------------------------------DECLARATION OF GAMEDATA VARIABLES--------------------------------------
 
@@ -30,10 +32,24 @@ var jobTitles = [
     "Royal abortionist",
     "Royal cumbucket cleaner",
     "the 'Queen'",
+    "BIG TOKE",
+    "Cumwizard",
+    "shitty artist",
+    "welfare queen",
+    "baby killer",
+    "pothead",
+    "child molestor (bad!)",
+    "catholic priest",
+    "slave",
+    "cum",
+    "Local peasant",
+    "Presidential candidate",
+    "yeast",
+
 ]
 const randomJobTitle = jobTitles[Math.floor(Math.random() * jobTitles.length)];
 var isEmployed = false;
-
+//--------------------------------------VARIABLES RELATING TO JOB TITLES, ETC-------------------------------------
 function takeAToke() {
         gameData.tokes += gameData.tokesPerClick;
         document.getElementById("tokesTaken").innerHTML = gameData.tokes;
@@ -60,10 +76,10 @@ function workShift() {
     gameData.wealth += gameData.salary;
     gameData.shiftsRemaining --;
     if (gameData.shiftsRemaining == 0) {
-        gameData.promotionTracker++;
+        gameData.promotionTracker ++; 
         const randomJobTitle = jobTitles[Math.floor(Math.random() * jobTitles.length)];
         document.getElementById("jobTitle").innerHTML = randomJobTitle;
-        gameData.shiftsRemaining = (60 * 2) * gameData.promotionTracker;
+        gameData.shiftsRemaining = (10 + gameData.promotionTracker);
         document.getElementById("shiftsRemaining").innerHTML = gameData.shiftsRemaining.toString();
     }
     document.getElementById("currentWealth").innerHTML = gameData.wealth.toString();
@@ -87,6 +103,17 @@ function buyBud() {
 }
 //--------------------------------------FUNCTION THAT BUYS AN EIGHTH--------------------------------------
 
+function buyAutoToker() {
+    if (gameData.joints >= gameData.autoTokeCost){
+        gameData.joints -= gameData.autoTokeCost;   
+        gameData.autoTokeCount += 1;
+        document.getElementById("jointCounter").innerHTML = gameData.joints.toString();
+        gameData.autoTokeCost += 2;
+        document.getElementById("autoTokeCost").innerHTML = gameData.autoTokeCost.toString();
+    }
+    document.getElementById("autoTokeCount").innerHTML = gameData.autoTokeCount.toString();
+}
+//--------------------------------------FUNCTION THAT BUYS AN AUTO-TOKER--------------------------------------
 
 var saveGameLoop = window.setInterval(function() {
     localStorage.setItem('tokeSeshSave', JSON.stringify(gameData));
@@ -121,16 +148,48 @@ var buttonPermissionLoop = window.setInterval(function() {
         document.getElementById("workShift").removeAttribute("disabled");
         document.getElementById("findJob").setAttribute("disabled","disabled");
     }
+    if (gameData.joints < gameData.autoTokeCost){
+        document.getElementById("buyAutoToke").setAttribute("disabled","disabled");
+    }
+    else {
+        document.getElementById("buyAutoToke").removeAttribute("disabled");
+    }
 }, 250)
 //--------------------------------------LOOP THAT DOES BUTTON PERMISSIONS--------------------------------------
 
+var victoryCheckLoop = window.setInterval(function() {
+    if (gameData.tokes >= 42069) {
+        document.getElementById("buyAutoToke").setAttribute("disabled", "disabled");
+        document.getElementById("takeAToke").setAttribute("disabled", "disabled");
+        document.getElementById("rollOne").setAttribute("disabled", "disabled");
+        document.getElementById("buyBud").setAttribute("disabled", "disabled");
+        document.getElementById("workShift").setAttribute("disabled", "disabled");
+        document.getElementById("victoryBanner").innerHTML = "VICTORY: 42069 TOKES ACHIEVED (YOU OVERDOSED ON THC...)";
+        gameData.tokes = 0;
+        gameData.tokesPerClick = 0;
+        gameData.tokeCounter = 0;
+        gameData.wealth = 0;
+        gameData.budLeft = 0;
+        gameData.joints = 0;
+        gameData.shiftsRemaining = -1;
+        gameData.salary = 8;
+        gameData.autoTokeCount = 0;
+        gameData.autoTokeCost = 0;
+        gameData.promotionTracker = 0;
+    }
+},500)
 
-function autoToker(){  
-    takeAToke();
-}
-//--------------------------------------WORK IN PROGRESS AUTO-TOKER LOOP--------------------------------------
+var autoTokeLoop = window.setInterval(function() {
+    gameData.tokes += (1 * gameData.autoTokeCount);
+    document.getElementById("tokesTaken").innerHTML = gameData.tokes;
+}, 500)
+//--------------------------------------AUTO-TOKER LOOP--------------------------------------
 
 
 var savegame = JSON.parse(localStorage.getItem("tokeSeshSave"))
 //--------------------------------------SAVEGAME VARIABLE--------------------------------------
 
+if (savegame !== null) {
+    gameData = savegame;
+}
+//--------------------------------------SAVEGAME ACTIVATOR--------------------------------------
